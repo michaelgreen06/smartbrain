017d6a2b2b91e955b46e81e9e9e9d3a3941404d8
Michael Green <jonnyblu1982@gmail.com>
2022-09-29 16:32:00 -0600

    udated w/ heroku server url

--- smartbrain/src/Components/Register/Register.js
+++ smartbrain/src/Components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component{
   }
   
   onSubmitSignIn=()=>{
-    fetch('http://localhost:3000/register',{
+    fetch('https://lit-brook-19601.herokuapp.com/register',{
       method:'post',
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify({
